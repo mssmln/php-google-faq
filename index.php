f970cfc36641952289fe5b56a0f6c614708c1271
Massimiliano <m.croci.ly@gmail.com>
2021-02-26 14:13:27 +0100

    init

---
+++ index.php
@@ -0,0 +1,50 @@
+<!-- Riscrivere questa pagina del sito google https://policies.google.com/faq.
+Ci sono diverse domande con relative risposte. Gestire il “Database” e la visualizzazione di queste domande e risposte con PHP. -->
+
+<?php
+  $faqs = [
+
+  ]
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;500&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="css/style.css">
+  <title>Google's FAQ with php</title>
+</head>
+<body>
+  <header>
+    <div class="top">
+      <h2>privacy &amp; terms</h2>
+
+    </div>
+    <div class="bottom">
+      <nav>
+        <ul>
+          <li>Overview</li>
+          <li>Privacy Policy</li>
+          <li>Terms of Service</li>
+          <li>Technologies</li>
+          <li>FAQ</li>
+
+        </ul>
+      </nav>
+
+    </div>
+
+  </header>
+
+  <main>
+    <section>
+
+    </section>
+  </main>
+
+</body>
+</html>
